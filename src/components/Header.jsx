995f3ee99e5f942bffa453bfada5d86789c6d0d8
pranesh200425 <danieltup10@gmail.com>
2026-01-12 16:10:55 +0530

    almost a finished prototype

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import '../index.css'
 
-const Header = () => {
+const Header = () => {  
+
   return (
-    <div className='w-full flex flex-col items-center justify-center p-2 ' >
-        <div className='bg-[#252525] pr-2 pl-2 pt-4 pb-4 rounded-4xl ' >
-            <div className='text-5xl' >
-                <p className='tracking-wider text-yellow-300' >MISSING AUTHENTIC BIRIYANI ???</p>
-            </div>
+    <div className="w-full flex flex-col items-center justify-center p-2 ">
+      <div className="bg-[#252525] pr-2 pl-2 pt-4 pb-4 rounded-4xl ">
+        <div className="text-5xl min-[440px]:text-7xl min-[768px]:text-[12rem]  ">
+          <p className="tracking-wider text-yellow-300" id="headerText">MISSING AUTHENTIC BIRIYANI??? </p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FoodCard from "./MicroComponents/FoodCard";
+import '../index.css'
 
 const Menu = () => {
   return (
     <div className="flex flex-col w-full " >
-      <h1 className="flex text-5xl font-bold pl-4" >Menu</h1>
+      <h1 className="flex text-5xl font-bold pl-4" id="menu" >Menu</h1>
       <div className="flex w-full pt-4 ">
         <FoodCard />
         <FoodCard />
