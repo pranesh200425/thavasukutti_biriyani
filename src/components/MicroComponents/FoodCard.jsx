8482d10721c2cb1d3c685e354232f29160b102fd
pranesh200425 <danieltup10@gmail.com>
2026-01-12 16:24:43 +0530

    mobile version is almost ready some minor tweaks to be made but we got it

--- src/components/MicroComponents/FoodCard.jsx
+++ src/components/MicroComponents/FoodCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bir1 from '../../assets/bir1.png' 
 import '../../App.css'
+import '../../index.css'
 
 const FoodCard = ({ foodimg = bir1, foodname = "Chicken biriyani", price = "Rs.170" , description = "Tasty gama gama biriyani" }) => {
   return (
@@ -9,7 +10,8 @@ const FoodCard = ({ foodimg = bir1, foodname = "Chicken biriyani", price = "Rs.1
         <img src={foodimg}  alt="foodImage" />
       </div>
       <div className="flex flex-col w-full p-4 absolute bottom-0" id="glass_bg" >
-        <h1 className='text-2xl font-semibold' >{foodname}</h1>
+        <h1 className='text-2xl font-bold' id='itemName' >{foodname}</h1>
+        <p id="description" >Tasty flavorful seeraga samba chicken biriyani</p>
         <h3 className="text-xl font-black text-white" >{price}</h3>
       </div>
     </div>
