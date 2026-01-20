3d8ea58a5ae9337f4c42e8ca846d449d192861b4
pranesh200425 <danieltup10@gmail.com>
2026-01-20 15:45:13 +0530

    did some changes in color for overall page and had changed the style of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import '../index.css'
 const Header = () => {  
 
   return (
-    <div className="w-full flex flex-col items-center justify-center p-2 ">
-      <div className="bg-[#252525] pr-2 pl-2 pt-4 pb-4 rounded-4xl ">
+    <div className="w-full flex flex-col items-center justify-center p-2 "  >
+      <div className="bg-[#e7e55a80] pr-2 pl-2 pt-4 pb-4 rounded-4xl " id='header-head' >
         <div className="text-5xl min-[440px]:text-7xl min-[768px]:text-[12rem]  ">
-          <p className="tracking-wider text-yellow-300" id="headerText">MISSING AUTHENTIC BIRIYANI??? </p>
+            <p className="tracking-wider drop-shadow-black text-[#5c4b00]" id="headerText">MISSING AUTHENTIC BIRIYANI??? </p>
         </div>
       </div>
     </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,7 +5,7 @@ import '../index.css'
 const Menu = () => {
   return (
     <div className="flex flex-col w-full " >
-      <h1 className="flex text-5xl font-bold pl-4" id="menu" >Menu</h1>
+      <div className="flex text-5xl font-bold pl-4 " id="menu" ><h1 className="underline decoration-4 underline-offset-8 decoration-[#7a5c00] text-[#5c4b00] " >Menu</h1></div>
       <div className="flex w-full pt-4 ">
         <FoodCard />
         <FoodCard />
