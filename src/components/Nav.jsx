473f2109dba1cc5f01430f9030e2f2201004351d
pranesh200425 <danieltup10@gmail.com>
2026-01-20 17:28:46 +0530

    test on mobile ignore these commits

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <>
-    <nav className='flex flex-col  sticky text-3xl p-2 m-2 '>
+    <nav className='flex flex-col  sticky text-2xl p-2 m-2 '>
         <h1 className='p-4 w-full text-center text-[#5c4b00] font-extrabold ' >தவுசுகுட்டி பிரியாணி</h1>
     </nav>
     </>
