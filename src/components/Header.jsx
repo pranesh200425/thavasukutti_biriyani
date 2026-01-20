2724bc02e6be6e90a8d2970d6db8010e4a2633e0
pranesh200425 <danieltup10@gmail.com>
2026-01-20 16:24:30 +0530

    test on mobile ignore these commits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center p-2 "  >
-      <div className="bg-[#e7e55a80] pr-2 pl-2 pt-4 pb-4 rounded-4xl " id='header-head' >
+      <div className="bg-[#c5c47d80] pr-2 pl-2 pt-4 pb-4 rounded-4xl " id='header-head' >
         <div className="text-5xl min-[440px]:text-7xl min-[768px]:text-[12rem]  ">
             <p className="tracking-wider drop-shadow-black text-[#5c4b00]" id="headerText">MISSING AUTHENTIC BIRIYANI??? </p>
         </div>
